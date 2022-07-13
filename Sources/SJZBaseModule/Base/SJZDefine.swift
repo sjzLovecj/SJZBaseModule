5aa612c7e94752a1dadf062254ed81eb51dfda5d
jiazuo.shao <jiazuo.shao@ikang.com>
2022-07-13 15:41:13 +0800

    新增获取当前控制器方法

--- Sources/SJZBaseModule/Base/SJZDefine.swift
+++ Sources/SJZBaseModule/Base/SJZDefine.swift
@@ -77,3 +77,34 @@ public var currentWindow: UIWindow? {
     let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
     return windowScene?.windows.last
 }
+
+func currentController() -> UIViewController? {
+    if let keyWindow = keyWindow {
+        if let rootVC = keyWindow.rootViewController, let nextResponder = rootVC.view.next {
+            if let vc = nextResponder as? UIViewController {
+                return topViewController(controller: vc)
+            }else {
+                return topViewController(controller: keyWindow.rootViewController)
+            }
+        }
+    }
+    
+    return nil
+}
+
+@discardableResult
+func topViewController(controller: UIViewController?) -> UIViewController? {
+    if let tabVC = controller as? UITabBarController {
+        return topViewController(controller: tabVC.selectedViewController)
+    }else if let navVC = controller as? UINavigationController {
+        topViewController(controller: navVC.topViewController)
+    }else if let presented = controller?.presentedViewController {
+        if let _ = presented as? UIAlertController {
+            return controller
+        }else {
+            topViewController(controller: presented)
+        }
+    }
+    
+    return controller
+}
